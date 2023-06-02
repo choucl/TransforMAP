ac852a3d20d62148da56db91f7118a334d5e42d3
choucl <clc25600@gmail.com>
2023-06-02 16:33:25 +0800

    Update training script

--- TransforMAP/train.sh
+++ TransforMAP/train.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-python ./TMAP_train.py $1 $2 $3
-#path_load_trace; model_save_path; num_instructions
+python ./TMAP_train.py $1 $2 $3 $4
+# path_to_load_json path_of_data_dir path_to_save_model_to ratio_of_training_data
 
 #train.sh /home/pengmiao/Disk/work/MLPrefComp/ChampSim/ML-DPC/LoadTraces/spec17/602.gcc-s2.txt.xz ./results/spec17/602.gcc-s2.model.pth 5
 
